66fde13c1e4dd53c9bcd246233759dbf5e2f8b0a
Iulian-Catalin <constantin.94.catalin@gmail.com>
2023-03-12 22:56:53 +0200

    1

--- src/Account/Login.java
+++ src/Account/Login.java
@@ -1,11 +1,15 @@
 package Account;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.concurrent.TimeUnit;
 
 public class Login {
@@ -44,74 +48,130 @@ public class Login {
                 }
             }
         }
-            while (!result) ;
-            return result;
+        while (!result);
+        return result;
 
     }
 
 
-        private List<User> loadAccounts () {
-            Path pIn = Paths.get("Accounts.txt");
-            List<User> listOfUsers = new ArrayList<>();
-            List<String> listOfUsersAsString = new ArrayList<>();
+    private List<User> loadAccounts() {
+        Path pIn = Paths.get("Accounts.txt");
+        List<User> listOfUsers = new ArrayList<>();
+        List<String> listOfUsersAsString = new ArrayList<>();
+        try {
+            listOfUsersAsString = Files.readAllLines(pIn);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int x = 0; x < listOfUsersAsString.size(); x++) {
+            User temp = new User();
+            String curentLne = listOfUsersAsString.get(x);
+            StringTokenizer st = new StringTokenizer(curentLne, ",");
+            while (st.hasMoreTokens()) {
+                String u = st.nextToken().trim();
+                String p = st.nextToken().trim();
+                String ad = st.nextToken().trim();
+                temp.setUser(u);
+                temp.setPassword(p);
+                if (ad.equalsIgnoreCase("true")) {
+                    temp.setAdministrator(true);
+                }
+            }
+            listOfUsers.add(temp);
+            System.out.println(listOfUsersAsString);
+        }
+        return listOfUsers;
+    }
+
+    public void createAccount() {
+        User a = new User();
+        System.out.print("\nInser username: ");
+        String user = new Scanner(System.in).nextLine();
+        a.setUser(user);
+        System.out.print("\nInser password: ");
+        String pwd = new Scanner(System.in).nextLine();
+        a.setPassword(pwd);
+        System.out.print("\nAdministrator type account: ");
+        boolean admin = new Scanner(System.in).nextBoolean();
+        a.setAdministrator(admin);
+        listOfUsers.add(a);
+        File os = Paths.get("Accounts.txt").toFile();
+        os.delete();
+
+        os = new File("Accounts.txt");
+        try {
+            os.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Path p = Paths.get("Accounts.txt");
+        String txt;
+        for (int x = 0; x < listOfUsers.size(); x++) {
+            if (x == listOfUsers.size() - 1) {
+                txt = listOfUsers.get(x).getUser() + ", " +
+                        listOfUsers.get(x).getPassword() + ", " +
+                        listOfUsers.get(x).isAdministrator();
+            } else txt = listOfUsers.get(x).getUser() + ", " +
+                    listOfUsers.get(x).getPassword() + ", " +
+                    listOfUsers.get(x).isAdministrator() + "\n";
             try {
-                listOfUsersAsString = Files.readAllLines(pIn);
+                Files.write(p, txt.getBytes(), StandardOpenOption.APPEND);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            for (int x = 0; x < listOfUsersAsString.size(); x++) {
-                User temp = new User();
-                String curentLne = listOfUsersAsString.get(x);
-                StringTokenizer st = new StringTokenizer(curentLne, ",");
-                while (st.hasMoreTokens()) {
-                    String u = st.nextToken().trim();
-                    String p = st.nextToken().trim();
-                    String ad = st.nextToken().trim();
-                    temp.setUser(u);
-                    temp.setPassword(p);
-                    if (ad.equalsIgnoreCase("true")) {
-                        temp.setAdministrator(true);
-                    }
-                }
-                listOfUsers.add(temp);
-                System.out.println(listOfUsersAsString);
-            }
-            return listOfUsers;
         }
+    }
 
-        public void createAccount (User p){
-            String user = p.getUser();
-            String password = p.getPassword();
-            boolean administrator = p.isAdministrator();
-            String newRow = "\n" + user + ", " + password + ", " + administrator;
-            Path pOut = Paths.get("Accounts.txt");
-            try {
-                Files.write(pOut, newRow.getBytes(), StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public void removeAccount () {
-            boolean result = false;
-            do {
-                System.out.print("Insert username: ");
-                String user = new Scanner(System.in).nextLine();
-                System.out.print("\nInsert password: ");
-                String password = new Scanner(System.in).nextLine();
-                User logged = new User(user, password);
-                boolean loggedIn = false;
-                for (User u : listOfUsers) {
-                    if (u.equals(logged)) {
-                        System.out.println("Account deleted !");
-                        result = true;
-                    }
-                }
-                if (!loggedIn) {
+    public void removeAccount() {
+        boolean result = false;
+        do {
+            System.out.print("Insert username: ");
+            String user = new Scanner(System.in).nextLine();
+            User del = new User();
+            boolean loggedIn = false;
+            for (User u : listOfUsers) {
+                if (u.getUser().equals(user)) {
+                    del.setUser(u.getUser());
+                    del.setPassword(u.getPassword());
+                    del.setAdministrator(u.isAdministrator());
+                    loggedIn = true;
+                } else {
                     System.out.println("Attempt failed !");
                 }
             }
-            while (!result);
-        }
+            if (loggedIn) {
+                listOfUsers.remove(del);
+                System.out.println("Account deleted !");
+                result = true;
 
+                File os = Paths.get("Accounts.txt").toFile();
+                os.delete();
+
+                os = new File("Accounts.txt");
+                try {
+                    os.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Path p = Paths.get("Accounts.txt");
+                String txt;
+                for (int x = 0; x < listOfUsers.size(); x++) {
+                    if (x == listOfUsers.size() - 1) {
+                        txt = listOfUsers.get(x).getUser() + ", " +
+                                listOfUsers.get(x).getPassword() + ", " +
+                                listOfUsers.get(x).isAdministrator();
+                    } else txt = listOfUsers.get(x).getUser() + ", " +
+                            listOfUsers.get(x).getPassword() + ", " +
+                            listOfUsers.get(x).isAdministrator() + "\n";
+                    try {
+                        Files.write(p, txt.getBytes(), StandardOpenOption.APPEND);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        while (!result);
     }
+
+}
--- src/Account/Menu.java
+++ src/Account/Menu.java
@@ -4,30 +4,20 @@ import java.util.Scanner;
 public class Menu {
     public void start() {
         Login on = new Login();
-        User a = new User();
         boolean logged = on.login();
         while (logged) {
-            System.out.println("Select from available options below:");
-            System.out.println("1. Create account;");
-            System.out.println("2. Logout and change account;");
-            System.out.println("3. Exit;");
-            System.out.print("Insert: ");
+            System.out.println("Select from available options below: " +
+                    "\n1. Create account;" +
+                    "\n2. Logout and change account;" +
+                    "\n3. Delete account;" +
+                    "\n4. Exit;" +
+                    "\nInsert: ");
             int insert = new Scanner(System.in).nextInt();
             switch (insert) {
-                case 1 -> {
-                    System.out.print("\nInser username: ");
-                    String user = new Scanner(System.in).nextLine();
-                    a.setUser(user);
-                    System.out.print("\nInser password: ");
-                    String pwd = new Scanner(System.in).nextLine();
-                    a.setPassword(pwd);
-                    System.out.print("\nAdministrator type account: ");
-                    boolean admin = new Scanner(System.in).nextBoolean();
-                    a.setAdministrator(admin);
-                    on.createAccount(a);
-                }
+                case 1 -> on.createAccount();
                 case 2 -> logged = on.login();
-                case 3 -> {
+                case 3 -> on.removeAccount();
+                case 4 -> {
                     logged = false;
                     System.out.println("Shuting down ...");
                 }
